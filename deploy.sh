22173f1b21252b10543206505605ff4fa7c58091
gokulsm1197 <141155162+gokulsm1197@users.noreply.github.com>
2024-03-17 04:02:14 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+echo "deployment start"
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+docker pull gokulsm/dev
+docker run -d -it -p 80:80 gokulsm/dev
+echo "deployment completed"
